945fcf937cd30f6ecb5bc366eb336629b84794b4
anjeh24 <junez24@yahoo.com>
2024-07-26 10:18:46 -0400

    created a for loop that counts from 1-10 which will be imported into another file and appended to html

---
+++ modules/module.js
@@ -0,0 +1,6 @@
+let x = 0;  //declaring a variable
+for (let i = 1; i < 11; i++){
+    x = i; // for loop to print out its values from 1 to 10
+    console.log(x); //console logging to check
+    
+}
